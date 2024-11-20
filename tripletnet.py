34142d8667f1fc96dc128bd32527a7f916d7f996
Ciprian Bangu <ciprian.bangu@gmail.com>
2024-11-21 00:44:37 +0100

    tripletnet start

---
+++ tripletnet.py
@@ -0,0 +1,43 @@
+import torch.nn as nn
+class BigTripletNet(nn.Module): #this one is designed to take in all the sensors as one big tensor
+    def __init__(self):
+        super(BigTripletNet, self).__init__()
+        
+        #Conv Blocks
+        self.conv1 = nn.Conv2d(in_channels=247, out_channels=16, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.act1 = nn.GELU()
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2) #(16, 24, 40)
+
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1) 
+        self.bn2 = nn.BatchNorm2d(32)
+        self.act2 = nn.GELU()
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2) #(32, 12, 20)
+
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.act3 = nn.GELU()
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2) #64, 6, 10
+
+        self.conv4 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
+        self.bn4 = nn.BatchNorm2d(128)
+        self.act4 = nn.GELU()
+        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2) #128, 3, 5
+
+        #FC layers
+        self.fc1 = nn.Linear(1920, 512)
+        self.fc2 = nn.Linear(512, 128)
+
+
+    def forward(self, x):
+        x = self.pool1(self.act1(self.bn1(self.conv1(x))))
+        x = self.pool2(self.act2(self.bn2(self.conv2(x))))
+        x = self.pool3(self.act3(self.bn3(self.conv3(x))))
+        x = self.pool4(self.act4(self.bn4(self.conv4(x))))
+
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        print(x.shape)
+
+        return x
