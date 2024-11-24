a8041a8f5f924f74ddbc009a51fe40ee4811fc5f
Ciprian Bangu <ciprian.bangu@gmail.com>
2024-11-24 22:49:18 +0100

    save

--- tripletnet.py
+++ tripletnet.py
@@ -6,22 +6,22 @@ class BigTripletNet(nn.Module): #this one is designed to take in all the sensors
         #Conv Blocks
         self.conv1 = nn.Conv2d(in_channels=247, out_channels=16, kernel_size=3, padding=1)
         self.bn1 = nn.BatchNorm2d(16)
-        self.act1 = nn.GELU()
+        self.act1 = nn.ReLU()
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2) #(16, 24, 40)
 
         self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1) 
         self.bn2 = nn.BatchNorm2d(32)
-        self.act2 = nn.GELU()
+        self.act2 = nn.ReLU()
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2) #(32, 12, 20)
 
         self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
         self.bn3 = nn.BatchNorm2d(64)
-        self.act3 = nn.GELU()
+        self.act3 = nn.ReLU()
         self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2) #(64, 6, 10)
 
         self.conv4 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
         self.bn4 = nn.BatchNorm2d(128)
-        self.act4 = nn.GELU()
+        self.act4 = nn.ReLU()
         self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2) #(128, 3, 5)
 
         #FC layers
