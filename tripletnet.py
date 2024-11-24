1bb449fcd9bde44d43b214baa1095a4161843093
Ciprian Bangu <ciprian.bangu@gmail.com>
2024-11-24 20:30:44 +0100

    save

--- tripletnet.py
+++ tripletnet.py
@@ -17,12 +17,12 @@ class BigTripletNet(nn.Module): #this one is designed to take in all the sensors
         self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
         self.bn3 = nn.BatchNorm2d(64)
         self.act3 = nn.GELU()
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2) #64, 6, 10
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2) #(64, 6, 10)
 
         self.conv4 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
         self.bn4 = nn.BatchNorm2d(128)
         self.act4 = nn.GELU()
-        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2) #128, 3, 5
+        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2) #(128, 3, 5)
 
         #FC layers
         self.fc1 = nn.Linear(1920, 512)
