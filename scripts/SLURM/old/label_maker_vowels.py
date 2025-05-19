558b6093e7f0796a3b855d34d049bf23fc8dacfe
Ciprian Bangu <ciprian.bangu@gmail.com>
2025-05-19 22:08:22 +0200

    script reorg

---
+++ scripts/SLURM/old/label_maker_vowels.py
@@ -0,0 +1,74 @@
+import os
+import csv
+import argparse
+
+def main():
+
+
+    parser= argparse.ArgumentParser(description="This script creates the dataset label csvs for the model training in the vowel discrimintaion regime")
+
+    parser.add_argument('--data_dir', type=str, required=True, help='directory the data is located in')
+    parser.add_argument('--save_dir', type=str, required=True, help='directory you want to save the labels in')
+    parser.add_argument('--res_dims', type=str, required=True, help='resolution and dimensions of the data contained, ie 224_16x16')
+    parser.add_argument('--producing_only', action="store_true", help='make labels only for producing samples')
+
+    args = parser.parse_args()
+    
+    # label dictionary - maybe it would be better to do a label2id but i set up all the scripts to work like this so i don't want to break anything now/
+    vowels = {
+        'a': 0, 
+        'e': 1, 
+        'i': 2
+    }
+
+    # combining vowels and consonants, but labeled by vowels
+    
+    if args.producing_only:
+        csv_file = os.path.join(args.save_dir, f'combined_labels_{args.res_dims}_producing_only.csv')
+    else:
+        csv_file = os.path.join(args.save_dir, f'combined_labels_{args.res_dims}_all.csv')
+    
+    scalogram_files = os.listdir(args.data_dir)
+    if args.producing_only:
+        scalogram_files = [f for f in scalogram_files if (len(f.split('_')[4]) >= 3)]
+
+    with open(csv_file, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['FileName', 'Label'])
+
+        for file_name in scalogram_files:
+            vowel_found = False
+            label = file_name.split('_')[3]
+            for vowel in vowels:
+                if vowel in label:
+                    label = vowels[vowel]
+                    writer.writerow([file_name, label])
+                    vowel_found = True
+                    break
+
+            if not vowel_found:
+                raise ValueError(f"no vowel found in file name {file_name}!")
+
+    # Vowels only
+    # All first then vowlels only
+    if args.producing_only:
+        csv_file = os.path.join(args.save_dir, f'vowels_only_labels_{args.res_dims}_producing_only.csv')
+    else:
+        csv_file = os.path.join(args.save_dir, f'vowels_only_labels_{args.res_dims}.csv')
+
+    with open(csv_file, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['FileName', 'Label'])
+
+        for file_name in scalogram_files:
+            label = file_name.split('_')[3]
+            if len(label) > 1:
+                continue
+            elif label in vowels:
+                writer.writerow([file_name, vowels[label]])
+            else:
+                raise ValueError(f"no vowel found in {file_name}! or filename formatted incorrectly. ")
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/SLURM/old/train_test_split_vowels.py
@@ -0,0 +1,85 @@
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from argparse import ArgumentParser
+
+# I think this should still work if we have ONLY producing samples. 
+
+def main():
+    parser = ArgumentParser(description="This script takes the labels and makes train_validation_test splits for the data in quesiton")
+
+    parser.add_argument('--data_dir', type=str, required=True, help="Path to where the label csvs are")
+    parser.add_argument('--save_dir', type=str, required=True, help="path where the output csvs will be stored")
+    parser.add_argument('--dimensions', type=str, required=True, help="the dimensions, formatted like they are in the labels file")
+    parser.add_argument('--class_tag', type=str, help="add the class tag in case it is an roi split; i.e. 3c or 2c")
+    
+    
+    args = parser.parse_args()
+
+    label_files = os.listdir(args.data_dir)
+    
+    for f in label_files:
+
+        print(f"processing {f}")
+        df = pd.read_csv(os.path.join(args.data_dir, f)).reset_index(drop=True)
+
+        total_samples = len(df)
+        print(f"Total samples: {total_samples}")
+
+        # we want 10% of total to be in the test set
+        test_amount = total_samples // 10
+
+        # but we need it only from the producing samples, so we need a lil df magic
+        # so take all of the producing samples out of df and put them in a new one
+        producing_df = pd.DataFrame() # empty df to hold producing 
+        for index, row in df.iterrows():
+            if len(row.iloc[0].split("_")[4]) >= 3:
+                producing_df = pd.concat([producing_df, pd.DataFrame([row])], ignore_index=True)
+                df.drop(index, inplace=True)
+        print(f"producing samples: {len(producing_df)}")
+        
+
+
+        # splitting to right percentages
+        percentage_for_test = test_amount/len(producing_df)
+        print(f"Percentage for test (or dataloss): {percentage_for_test}")
+
+
+        train_df, test_df = train_test_split(producing_df, test_size=percentage_for_test, random_state=42)
+        
+        df_identifiers = set(df.iloc[:, 0])  # first column is a unique identifier
+        test_df_identifiers = set(test_df.iloc[:, 0])
+
+        contamination = df_identifiers.intersection(test_df_identifiers)
+        if contamination:
+            print(f"WARNING: Data contamination detected! {len(contamination)} overlapping samples found between df and test_df.")
+            print(contamination)
+            raise ValueError("Data contamination detected between training and test sets!")
+
+        # now we add the producing samples that we train on back to the dataframe
+        df = pd.concat([df, train_df], ignore_index=True) 
+        print(f"Total samples in the new train dataset: {len(df)}")
+
+
+        # Saving to new CSV files
+        unpacked_filename = f.split("_")
+        if "producing" in unpacked_filename:
+            filename_train = f"{unpacked_filename[0]}_producing_{args.dimensions}_Kfold_train.csv"
+            filename_test = f"{unpacked_filename[0]}_producing_{args.dimensions}_test.csv"
+        else:
+            filename_train = f'{unpacked_filename[0]}_all_{args.dimensions}_Kfold_train.csv'
+            filename_test = f'{unpacked_filename[0]}_all_{args.dimensions}_test.csv'
+        
+        if args.class_tag:
+            filename_train = args.class_tag + "_" + filename_train
+            filename_test = args.class_tag + "_" + filename_test
+
+        df.to_csv(os.path.join(args.save_dir, filename_train), index=False)  # 90% of data
+        test_df.to_csv(os.path.join(args.save_dir, filename_test), index=False)    # 10% of data
+
+        print(f"Training set: {len(df)} samples")
+        print(f"Test set: {len(test_df)} samples")
+
+
+if __name__ == "__main__":
+    main()
